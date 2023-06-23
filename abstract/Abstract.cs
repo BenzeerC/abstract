6938c5e52001fc2f97414d91bfc164d6434e8c84
Benzeer <Benzeer@LAPTOP-HD4L2JBS>
2023-06-23 15:59:39 +0530

    Add project files.

---
+++ abstract/Abstract.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractclass
+    
+{
+    public interface IPrintDatials
+    {
+        void PrintDatials();
+    }
+    abstract public class Shape
+    {
+        public Shape()
+        {
+        }
+
+        public abstract void CalculateArea();
+
+        public string Display()
+        {
+            return "In the abstract class:";
+        }
+
+    }
+    public class Circle : Shape, IPrintDatials
+    {
+        private int r;
+        private double area;
+
+        public Circle(int r)
+        {
+            this.r = r;
+        }
+
+        public override void CalculateArea()
+        {
+            area = 3.14 * r * r;
+        }
+
+        public void PrintDatials()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"Area is:{area}";
+        }
+
+
+    }
+    public class Rectangle : Shape, IPrintDatials
+    { 
+            private int l, b,area;
+
+        public Rectangle(int l, int b)
+        {
+            this.l = l;
+            this.b = b;
+        }
+        public override void CalculateArea()
+        {
+            area = 2 *(l + b);
+        }
+
+        public void PrintDatials()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"Area of Rectangle is:{area}";
+        }
+       
+    }
+}
---
+++ abstract/Program.cs
@@ -0,0 +1,24 @@
+﻿using abstractclass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle  c1 = new Circle(5);
+            c1.CalculateArea();
+            Console.WriteLine(c1.ToString());
+           // Console.WriteLine(c1.PrintDatials());
+
+            Rectangle r1 = new Rectangle(7, 8);
+            r1.CalculateArea();
+            Console.WriteLine(r1.ToString());
+        }
+    }
+}
